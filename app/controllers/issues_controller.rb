17ec391d9d1bd136e950aa3c35872226192f66e1
lwskw <1207291903@qq.com>
2017-05-07 16:42:11 +0800

    CRUD

---
+++ app/controllers/issues_controller.rb
@@ -0,0 +1,10 @@
+class IssuesController < ApplicationController
+  def show
+	@issue = Issue.find(params[:id])
+  end
+  def destroy
+ 	 i = Issue.find(params[:id])
+ 	 i.destroy
+  	redirect_to :root
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'issues/show'
+
   get 'page/welcome'
 
 	root'page#welcome'
   get '/about'=>'page#about'
+  get 'issues/:id' => "issues#show", :as => "issue"
+  delete 'issues/:id' => 'issues#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
