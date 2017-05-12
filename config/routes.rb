6299fee187d83476c54142117a1f8d13c2e69a2a
lwskw <1207291903@qq.com>
2017-05-12 22:31:04 +0800

    rails 10days and over now

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'issues/show'
 
   get 'page/welcome'
 
 	root'page#welcome'
+
   get '/about'=>'page#about'
-  get 'issues/:id' => "issues#show", :as => "issue"
-  delete 'issues/:id' => 'issues#destroy'
+
+  resources :issues
+  # comments
+  post '/issues/:issue_id/comments' => "comments#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,5 +17,12 @@ ActiveRecord::Schema.define(version: 20170506030204) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
+  create_table "comments", force: true do |t|
+    t.text     "contect"
+    t.string   "username"
+    t.string   "email"
+    t.interger "issue_id"
+    t.datatime "created_at"
+    t.datetime "updated_at"
+  end
 end
